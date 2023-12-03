484b5044522c4b73155355096cbea79625e154d5
denisjosemendozavidal <denismendozavidal@gmail.com>
2023-12-03 11:57:09 -0500

    Starting - FireBase is set, needs testing

--- src/pages/api/firebase/firebase.tsx
+++ src/pages/api/firebase/firebase.tsx
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, FirebaseApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, Firestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
+export const db: Firestore = getFirestore(firebaseApp);
+
+export default firebaseApp;
