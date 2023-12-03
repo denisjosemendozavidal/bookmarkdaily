3bc7c8260b5aca1a422053da80366fb42272732f
denisjosemendozavidal <denismendozavidal@gmail.com>
2023-12-03 11:56:44 -0500

    Starting - FireBase is set, needs testing

---
+++ src/pages/api/firebase/firebase.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, FirebaseApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQA47I6tZg-BhfN0mBUEHHcgVpD9akyVs",
+  authDomain: "bookmarkdailydenis.firebaseapp.com",
+  projectId: "bookmarkdailydenis",
+  storageBucket: "bookmarkdailydenis.appspot.com",
+  messagingSenderId: "506285069956",
+  appId: "1:506285069956:web:edf9ec3099f8adf0b0d4d1",
+};
+
+// Initialize Firebase
+const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
